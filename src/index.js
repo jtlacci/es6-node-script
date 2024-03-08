080bbd1d1698ccfa965c9e42662535c43ea7c7d2
John Sterlacci <johntsterlacci@gmail.com>
2024-03-08 18:26:46 -0500

    feat: init commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+async function setup() {}
+
 /* Script Entry */
+async function main() {
+  // const { db, client } = await setup();
+}
+
+main().then(() => {
+  console.log("END");
+  return process.exit(1);
+});
